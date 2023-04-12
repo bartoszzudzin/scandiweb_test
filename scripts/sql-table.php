9bc5af01132af578d93652d6937d6bbaa1d910ef
Bartosz Zudzin <128415246+bartoszzudzin@users.noreply.github.com>
2023-04-12 11:13:26 +0200

    Update sql-table.php

--- scripts/sql-table.php
+++ scripts/sql-table.php
@@ -2,17 +2,17 @@
 
 require "connection.php";
 
-$sql = "CREATE TABLE `products` (
+$sql = "CREATE TABLE products(
     `sku` varchar(255) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `price` decimal(11,2) UNSIGNED NOT NULL,
-    `productType` varchar(255) NOT NULL,
-    `size` decimal(11,2) UNSIGNED DEFAULT NULL,
-    `height` decimal(11,2) UNSIGNED DEFAULT NULL,
-    `width` decimal(11,2) UNSIGNED DEFAULT NULL,
-    `length` decimal(11,2) UNSIGNED DEFAULT NULL,
-    `weight` decimal(11,2) UNSIGNED DEFAULT NULL
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+  `name` varchar(255) NOT NULL,
+  `price` decimal(11,2) UNSIGNED NOT NULL,
+  `productType` varchar(255) NOT NULL,
+  `size` varchar(5) NOT NULL,
+  `height` varchar(5) NOT NULL,
+  `width` varchar(5) NOT NULL,
+  `length` varchar(5) NOT NULL,
+  `weight` varchar(5) NOT NULL
+    )";
     
 if(mysqli_query($con, $sql)){
     echo "Table created!";
